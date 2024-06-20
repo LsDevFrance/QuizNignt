351358a53561536a3f667026afc85dda2cac6f6b
lsDevPlateforme <liron.saiman@laplateforme.io>
2024-06-20 10:58:38 +0200

    Ajouter un suspense pour la section QuizSectionCards dans la page marketing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/marketing/hero";
 import QuizSectionCards from "@/components/marketing/pricing";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <QuizSectionCards />
+      <Suspense fallback={<div>Loading...</div>}>
+        <QuizSectionCards />
+      </Suspense>
     </>
   );
 }
